739e90a1a5ba21542dfc97b67ec5b648b8d51bec
Nicolas Mandel <nicolasjohann.mandel@hdr.qut.edu.au>
2020-05-29 10:08:07 +1000

    Issue tf broadcaster - class change v1.1

--- tf_nodes/base_link_tf2_broadcaster.cpp
+++ tf_nodes/base_link_tf2_broadcaster.cpp
@@ -1,16 +1,17 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
-// #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <aruco_client/base_link_broadcaster.h>
+#include "aruco_client/base_link_broadcaster.h"
+#include <tf2/transform_datatypes.h>
 
 positionbrc::positionbrc(ros::NodeHandle nh)
 {   
-    ros::topic::waitForMessage<geometry_msgs::PoseStampedPtr>(pose_topic, nh, ros::Duration(10.0));
+    ros::topic::waitForMessage<geometry_msgs::PoseStamped>(pose_topic, nh, ros::Duration(10.0));
     tfs = geometry_msgs::TransformStamped();
-    tfs.header.frame_id = world_frame;
-    tfs.child_frame_id = child_frame;
+    // tfs.header.frame_id = world_frame;
+    // tfs.child_frame_id = child_frame;
     tbr = tf2_ros::TransformBroadcaster();   
     pose_subscriber = nh.subscribe(pose_topic,3, &positionbrc::poseCallback, this);
 }
@@ -23,7 +24,10 @@ void positionbrc::poseCallback(const geometry_msgs::PoseStampedConstPtr& msg)
 {
     try
     {
-        tfs.header.stamp = ros::Time::now();
+	    // tfs = geometry_msgs::TransformStamped();
+        tfs.header = msg->header;
+        tfs.header.frame_id = world_frame;
+        tfs.child_frame_id = child_frame;
         tfs.transform.translation.x = msg->pose.position.x;
         tfs.transform.translation.y = msg->pose.position.y;
         tfs.transform.translation.z = msg->pose.position.z;
@@ -32,11 +36,12 @@ void positionbrc::poseCallback(const geometry_msgs::PoseStampedConstPtr& msg)
         tfs.transform.rotation.z = msg->pose.orientation.z;
         tfs.transform.rotation.w = msg->pose.orientation.w;
 
+        //tf2::fromMsg(msg, tfs);
         tbr.sendTransform(tfs);
     }
     catch(ros::Exception& e)
     {
-       ROS_INFO(e.what());
+       ROS_INFO("%s", e.what());
     }
 }
 
@@ -50,7 +55,7 @@ int main(int argc, char** argv){
     }
     catch(const ros::Exception& e)
     {
-        ROS_INFO(e.what());
+        ROS_INFO("%s", e.what());
     }
 
     ros::spin();
